0710d5fe01c74f25eaa5c695ade7a5d436ec032e
Surya tej <medasuryatej@gmail.com>
2022-04-10 20:45:45 -0400

    Time: 186 ms (77.72%), Space: 14.2 MB (61.56%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,5 +1,6 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        """
         from itertools import chain
         total_size = len(grid) * len(grid[0])
         k = k % total_size
@@ -10,3 +11,16 @@ class Solution:
         # output = [shifted[i:i+len(grid[0])] for i in range(0, len(shifted), len(grid[0]))]
         output = [shifted[i: i + chunk_size] for i in range(0, len(shifted), chunk_size)]
         return output
+        """
+        aux = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+        rows = len(grid)
+        cols = len(grid[0])
+        k = k % (rows * cols)
+        for r in range(rows):
+            for c in range(cols):
+                if c + k < cols:
+                    aux[r][c+k] = grid[r][c]
+                else:
+                    d, m = divmod(c+k, cols)
+                    aux[(r + d)%rows][m] = grid[r][c]
+        return aux
