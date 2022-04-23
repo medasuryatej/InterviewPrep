55af1d2c206e84e4aac8da6b8e4bb108ab8e6bf3
Surya tej <medasuryatej@gmail.com>
2022-04-23 16:24:39 -0400

    Time: 140 ms (66.52%), Space: 15.4 MB (56.98%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        # create a dp array
+        # on first day the profit is zero
+        # when zero transactions can be be made the profit is zero
+        dp = [[0 for i in range(len(prices))] for j in range(k+1)]
+        # have a variable that keeps track of maxdiff between profit and current trans
+        
+        # iterate over every transaction
+        for trans in range(1, k+1):
+            # iterate over every price
+            maxDiff = 0 - prices[0]
+            for day in range(1, len(prices)):
+                
+                # if transaction is made the profit would be current day price
+                # plus the maxdiff
+                dp[trans][day] = max(prices[day] + maxDiff, dp[trans][day-1])
+                # Either keep holding the stock we bought before, or just buy in today
+                maxDiff = max(maxDiff, dp[trans-1][day-1] - prices[day])
+        for row in dp:
+            print(row)
+        # return the last element
+        return dp[-1][-1]
