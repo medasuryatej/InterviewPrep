2bf6e30866550ba2b53a1c114bb4ae2900b98e0b
Surya tej <medasuryatej@gmail.com>
2022-05-02 20:36:12 -0400

    Time: 258 ms (61.27%), Space: 15.2 MB (90.78%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -1,5 +1,8 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
+        """
+        # NlogN solution
+        
         sorted_nums = sorted(nums)
         start = len(nums)-1
         end = 0
@@ -8,3 +11,28 @@ class Solution:
                 start = min(start, i)
                 end = max(end, i)
         return end - start + 1 if end - start > 0 else 0
+        """
+        minE = float("inf")
+        maxE = float("-inf")
+        flag = False
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                flag = True
+            if flag:
+                minE = min(minE, nums[i])
+                
+        flag = False
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] > nums[i+1]:
+                flag = True
+            if flag:
+                maxE = max(maxE, nums[i])
+            
+        for i in range(0, len(nums)):
+            if minE < nums[i]:
+                break
+                
+        for j in range(len(nums)-1, -1, -1):
+            if maxE > nums[j]:
+                break
+        return j - i + 1 if j - i > 0 else 0
