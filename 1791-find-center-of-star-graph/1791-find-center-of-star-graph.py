cd122b8516f2a8118319dd789d567cff91028e13
Surya tej <medasuryatej@gmail.com>
2022-05-19 12:16:14 -0400

    Time: 981 ms (57.39%), Space: 49.5 MB (97.71%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,5 +1,6 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
+        """
         visit = set()
         for u, v in edges:
             if u in visit:
@@ -8,3 +9,10 @@ class Solution:
                 return v
             visit.add(u)
             visit.add(v)
+        """
+        firstNode = edges[0]
+        secondNode = edges[1]
+        if firstNode[0] in secondNode:
+            return firstNode[0]
+        else:
+            return firstNode[1]
