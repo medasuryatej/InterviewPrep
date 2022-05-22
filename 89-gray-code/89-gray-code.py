fa122331a31ef18843292f10244ada492fc4db89
Surya tej <medasuryatej@gmail.com>
2022-05-21 22:10:37 -0400

    Time: 469 ms (7.13%), Space: 24.8 MB (8.06%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -1,5 +1,21 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
+        """
+        n = 1 output [0, 1]
+        n = 2 output [00, 01, 11, 10 ]
+        n = 3 output [000, 001, 011, 010, 110, 111, 101, 100]
+        
+        -------
+        Half Zeros, One fourth Zeros,  --> next One eight and so on
+                    One fourth Ones,
+                       (flip the order)
+        
+        Half Ones   One fourth Ones,
+                    One fourth Zeros
+        
+        The pattern continues.
+        
+        """
         
         if n == 1:
             return [0, 1]
@@ -26,20 +42,29 @@ class Solution:
             # update the ones
             while startingPos < totalBits:
                 if counter == numConsecTrues:
+                    # increase the offset
                     startingPos += numConsecTrues
+                    # reset the counter
                     counter = 0
                 else:
+                    # flip the bit by one
                     bitPos[bit][startingPos] = 1
+                    # increment the counter
                     counter += 1
+                    # increment the positon
                     startingPos += 1
                     
         # for _ in bitPos:
         #    print(_, bitPos[_])
                     
         output = []
+        # merging the outputs
         for bits in zip(*bitPos.values()):
+            # map the bits to string
             numTostr = list(map(str, bits))
+            # join the string
             binRep = "".join(numTostr)
+            # convert bin string to int
             output.append(int(binRep, 2))
             
         return output
