8904cb4b4de97cee955d049af0de8ae7f2ed1736
Surya tej <medasuryatej@gmail.com>
2022-05-29 01:22:28 -0400

    Time: 8764 ms (33.33%), Space: 44.1 MB (100.00%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,20 +1,36 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        seen = set()
-        deq = [(0, 0, 0)]
+        # get rows and cols information
+        rows, cols = len(grid), len(grid[0])
+        # a set to keep track of visited elements
+        visited = set()
+        # a deq to traverse / bfs
+        deq = [(0, (0, 0))] # (obstacles_removed, x_cord, y_cord)
+        # four directions
+        directions  = ([1, 0], [-1, 0], [0, 1], [0, -1])
+        # while deq is not empty perform bfs
         while deq:
-            rem, x, y = heappop(deq)
-            if (x, y) in seen:
+            # fetch the min
+            minObstRem, (x, y) = heappop(deq) # pops that coords with minimum value in first coordinate i.e, minObstRem
+            # check if node already visited
+            if (x, y) in visited:
+                # skip
                 continue
-            seen.add((x, y))
+            # add it to the visited set
+            visited.add((x, y))
+            # check if we reached destination
             if (x, y) == (rows-1, cols-1):
-                return rem
-            for i, j in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                new_x, new_y = x+ i, y + j
-                if 0 <= new_x < rows and 0 <= new_y < cols and (new_x, new_y) not in seen:
-                    heappush(deq, (grid[new_x][new_y] + rem, new_x, new_y))
-        return rem
-                    
+                # return the minimum number of obstacles removed
+                return minObstRem
+            # traverse all four directions
+            for i, j in directions:
+                # update coords
+                new_x, new_y = x + i, y + j
+                # check the boundaries and visit set
+                if 0 <= new_x < rows and 0 <= new_y < cols and (new_x, new_y) not in visited:
+                    # add the new coord to the heapify dep
+                    # note grid[new_x][new_y] => if obstacle then it adds 1 to the minObstRem else 0
+                    heappush(deq, (grid[new_x][new_y] + minObstRem, (new_x, new_y)))
+        # unreachable
+        return -1
             
