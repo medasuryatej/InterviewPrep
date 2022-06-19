ba47c9315f8dc13e42183a8de7f8a5086b2a2b77
Surya tej <medasuryatej@gmail.com>
2022-06-19 15:16:32 -0700

    Time: 84 ms (57.14%), Space: 13.9 MB (42.86%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -1,13 +1,24 @@
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
+        # keeping track current sum achieved from string formation
         curSum = 0
+        # pointer to keep track of bit position
         pointer = 0
+        # iterate from end position
         for bit in s[::-1]:
+            # it bit is zero consider it, since it is allowed to have 
+            # any number of leading zeros
             if bit == "0":
+                # increment the pointer
                 pointer += 1
             else:
+                # if the bit is one, obtain how much value it would 
+                # add to the curSum
                 curValue = 2**pointer
+                # if the newsum doesn't exceed k value, the bit one can 
+                # be considered, else ignore
                 if curSum + curValue <= k:
                     curSum += curValue
                     pointer += 1
+        # return the current pointer position.
         return pointer
