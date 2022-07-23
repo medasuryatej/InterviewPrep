b9f0c91166c51796faccb5fce326503c2b03495c
Surya tej <medasuryatej@gmail.com>
2022-07-22 23:27:24 -0700

    Time: 3238 ms (76.08%), Space: 31.5 MB (93.57%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        # implement Binary Index Tree
+        def update(index, value, tree, size):
+            index += 1  # index in BIT is 1 more than the original index
+            while index < size:
+                tree[index] += value
+                index += index & -index
+
+        def query(index, tree):
+            # return sum of [0, index)
+            result = 0
+            while index >= 1:
+                result += tree[index]
+                index -= index & -index
+            return result
+
+        offset = 10**4  # offset negative to non-negative
+        size = 2 * 10**4 + 2  # total possible values in nums plus one dummy
+        tree = [0] * size
+        result = []
+        for num in reversed(nums):
+            smaller_count = query(num + offset, tree)
+            result.append(smaller_count)
+            update(num + offset, 1, tree, size)
+        return reversed(result)
