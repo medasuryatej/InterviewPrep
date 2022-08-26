b92a0c6702a33a83b2aa34325f8437fd02bb8055
Surya tej <medasuryatej@gmail.com>
2022-08-26 12:58:29 -0400

    Time: 6422 ms (5.23%), Space: 61.5 MB (17.13%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,5 +1,5 @@
 class Solution:
-    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+    def countUnguarded_DFS(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         dp = [[0] * n for _ in range(m)]
         for x, y in guards+walls:
             dp[x][y] = 1
@@ -17,3 +17,28 @@ class Solution:
                     dp[curr_x][curr_y] = 2
                     
         return sum(1 for i in range(m) for j in range(n) if dp[i][j] == 0)
+    
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+            '''## Approach : BFS ##'''
+            rows , cols = m , n
+            directions  = [(0,1),(0,-1),(1,0),(-1,0)]
+            
+            ## for efficiently search walls and gates
+            wallsAndGuards = set()
+            for r,c in walls+guards :
+                wallsAndGuards.add((r,c))
+            
+            queue = deque()
+            for r,c in guards :
+                for dr,dc in directions :   # go to all the directions for the curr guard
+                    queue.append([r,c,dr,dc])
+                    
+            visited = set()
+            while queue :
+                r,c,dr,dc = queue.popleft()
+                visited.add((r,c))  
+                newR, newC = r+dr, c+dc
+                if newR<0 or newR>rows-1 or newC<0 or newC>cols-1 or (newR,newC) in wallsAndGuards:
+                    continue
+                queue.append( [newR,newC,dr,dc] )
+            return m*n - len(visited) - len(walls)     # guard is already added in visitedSet
