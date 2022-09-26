a6bbfc54ba146d7a3afa5277eda0edce60d873fe
Surya tej <medasuryatej@gmail.com>
2022-09-25 23:07:23 -0400

    Time: 1 ms (100.00%), Space: 40.7 MB (98.85%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,16 +1,19 @@
 class Solution {
+    // referred solution
+    // you do a union find
+    // have a static array for each character
     static int[] root = new int[26];
     public boolean equationsPossible(String[] equations) {
         
         for (int i=0; i<26; i++) {
-            root[i] = i;
+            root[i] = i; // let parent of each character be itself
         }
      
         
         int x, y;
         
         for (String eq: equations) {
-            if (eq.charAt(1) == '=') {
+            if (eq.charAt(1) == '=') { // consider all equalilty equations being connected components, hence their parents being same
                 x = eq.charAt(0) - 'a';
                 y = eq.charAt(3) - 'a';
                 union(x, y);
@@ -18,10 +21,12 @@ class Solution {
         }
         
         for (String eq: equations) {
+            // for unequal components their parents must not be same
             if (eq.charAt(1) != '=' ) {
                 x = eq.charAt(0) - 'a';
                 y = eq.charAt(3) - 'a';
                 if (find(x) == find(y)) {
+                    // if same return false
                     return false;
                 }
             }
