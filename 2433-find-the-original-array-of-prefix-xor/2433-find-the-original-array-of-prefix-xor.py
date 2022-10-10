f32d35042efae92370861e6117b6270c8738adb3
Surya tej <medasuryatej@gmail.com>
2022-10-09 22:03:25 -0400

    Time: 1115 ms (50.00%), Space: 30.3 MB (83.33%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -21,7 +21,7 @@ class Solution:
             prev ^= output[-1]
         return output
     
-    def findArray(self, pref):
+    def _findArray(self, pref):
         if len(pref) == 1:
             return pref
         output = [pref[0]]
@@ -29,6 +29,9 @@ class Solution:
         for i in range(1, len(pref)):
             output.append(pref[i] ^ pref[i-1])
         return output
+    
+    def findArray(self, A):
+        return map(xor, A, [0] + A[:-1])
                 
         
         
